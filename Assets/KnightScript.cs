dbee40991e1ee07dfb0239c7321999296f3cb815
Nathan Wiatrek <nwiatrek@gmail.com>
2020-02-25 21:48:36 -0600

    assests and also starting

---
+++ Assets/KnightScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnightScript : MonoBehaviour
+{
+    public GameObject[] PathNode;
+    public int Health;
+    public int Speed;
+    public int Damage;
+
+    public int CoinsGained;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Health < 1)
+        {
+            OnDeath();
+        }
+        
+            transform.position += transform.right * Time.deltaTime * Speed;
+        
+    }
+
+    void OnDeath()
+    {
+
+    }
+}
